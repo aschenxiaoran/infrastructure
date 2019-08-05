afd8c3c0c06e913b0cb97c4948958e769e1ab712
qinshuangshuang <qinshuangshuang@shouqiev.com>
2019-08-05 17:24:15 +0800

    test

---
+++ xiaoran-infrastructure-springs/src/test/java/com/xiaoran/infrastructure/springs/XiaoranInfrastructureSpringsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xiaoran.infrastructure.springs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XiaoranInfrastructureSpringsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
